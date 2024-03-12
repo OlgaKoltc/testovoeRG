170c6cf73e58181b715363855bd3d932642ce726
OlgaKoltc <Olga.Koltc@gmail.com>
2024-03-12 23:15:32 +0300

    add components

---
+++ app/src/components/PostItem.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const PostItem = () => {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((result) => setPost(result));
+  }, [id]);
+
+  return (
+    <div className="container">
+      {post && (
+        <>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </>
+      )}
+      <Link to={"/"}>
+        <button>Назад к постам</button>
+      </Link>
+    </div>
+  );
+};
+export default PostItem;
